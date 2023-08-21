65bbaa110fd766e443c8b4e5a32803d307f3dd5b
Vaibhav Nimesh <vaibhavnimesh1@gmail.com>
2023-08-21 20:33:02 +0530

    Project_1

---
+++ chaiaurcode.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+
+    body.style.backgroundColor = e.target.id;
+    // if (e.target.id == "grey") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id == "white") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id == "blue") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id == "yellow") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+  });
+});
